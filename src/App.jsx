5f28a21793e372dec9abaf668df70e16f5a3301e
carlos <pachecoc010@gmail.com>
2023-04-13 16:35:26 -0400

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
 import './App.css'
-
+import { BookingButton } from './components/BookingButton'
+import MovieMain from './components/MovieMain'
+import { TopCastContainer } from './components/TopCastContainer'
 function App() {
-  
 
   return (
     <div className="App">
-      hola!
+        <MovieMain/>
+        <TopCastContainer/>
+        <BookingButton/>
     </div>
   )
 }
